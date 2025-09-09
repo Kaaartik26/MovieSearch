3dddb031d46fe8fe05659d12c96e83ebdc0ca241
unknown <kamatkartik2006@gmail.com>
2025-09-09 22:55:08 +0530

    xyz

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,18 @@
-const API_KEY = "075a163a3c900759fc83997b214e6142"
+const apiKey = import.meta.env.VITE_API_KEY;
+console.log(apiKey); 
+
+
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${apiKey}`);
   const data = await response.json();
   return data.results;
 };
 
 export const searchMovies = async (query) => {
   const response = await fetch(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+    `${BASE_URL}/search/movie?api_key=${apiKey}&query=${encodeURIComponent(
       query
     )}`
   );
